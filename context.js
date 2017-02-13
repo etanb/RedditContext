81e1e14d5196be375ded6dd3ef697b9354037b9f
renagadex2 <etan.berkowitz@gmail.com>
2017-02-12 23:59:42 -0800

    charts now load on hover, bomb gets added to all comments

--- context.js
+++ context.js
@@ -1,131 +1,150 @@
-function attachEventListenersToUsers() {
-  $("[data-author] .tagline")[1].getElementsByClassName("author")[0].text
-  var currentUserTagline,
-      currentUsername,
-      contentBox,
-      userContext;
-
-  for (var i = 0, lengthOfUsers = $("[data-author] .tagline").length; i < lengthOfUsers; i++) {
-    currentUserTagline = $("[data-author] .tagline")[i];
-    currentUsername = currentUserTagline.getElementsByClassName("author")[0].text;
-
-    contentBox = document.createElement('span');
-    contentBox.className = "usercontext";
-    contentBox.innerHTML = "💣";
-
-    userContext = document.createElement('span');
-    userContext.className = "tooltiptext";
-
-    getRedditUserData(currentUsername, currentUserTagline, contentBox, userContext);
-  }
-}
-
-function getRedditUserData(username, currentUserTagline, contentBox, userContext) {
-  var overviewData = [],
-      upvotedData = [],
-      savedData = [];
-
-  $.when(
-    $.get("https://www.reddit.com/user/" + username + "/overview.json?limit=10", function(overview, status){
-        if(status === "success") {
-          overviewData = overview.data.children;
-        }
-      })
-
-    // $.get("https://www.reddit.com/user/" + username + "/upvoted.json?limit=10", function(upvoted, status){
-    //     if(status === "success") {
-    //       upvotedData = upvoted.data.children;
-    //     }
-    //   }),
-
-    // $.get("https://www.reddit.com/user/" + username + "/saved.json?limit=10", function(saved, status){
-    //     if(status === "success") {
-    //       savedData = saved.data.children;
-    //     }
-    //   })
-  ).then( function(){
-    var chartArea = document.createElement("canvas");
-    chartArea.width = "300";
-    chartArea.height = "300";
-
-    var chartUserData = parseUserDataForSubreddits(overviewData.concat(upvotedData, savedData));
-
-    var subbredditChart = new Chart(chartArea, {
-      type: 'pie',
-      data: {
-          labels: chartUserData[0],
-          datasets: [
-              {
-                  data: chartUserData[1],
-                  backgroundColor: [
-                "#F9C80E",
-                "#F86624",
-                "#EA3546",
-                "#662E9B",
-                "#43BCCD",
-                "#3D315B",
-                "#444B6E",
-                "#708B75",
-                "#9AB87A",
-                "#F8F991"
-
-            ]
-              }
-          ]
-        },
-      options: { responsive: false }
-    })
-
-    userContext.appendChild(chartArea)
-    contentBox.appendChild(userContext);
-    currentUserTagline.appendChild(contentBox);
-  })
-}
-
-function parseUserDataForSubreddits(userData) {
-  var userActiveSubreddit = [],
-      currentSubreddit,
-      activeSubredditString = "";
-
-  for(var i = 0, userDataLength = userData.length; i < userDataLength; i++) {
-    currentSubreddit = userData[i].data.subreddit;
-    
-    if(!checkIfInNestedArrayAndIncrementIfTrue(userActiveSubreddit, currentSubreddit)) {
-      userActiveSubreddit.push([currentSubreddit, 1]);
-    }
-  }
-
-  userActiveSubreddit.sort(function(a, b){return a[1] < b[1]})
-  if(userActiveSubreddit.length) {
-    var userSubreddit = [],
-        userCount = [];
-    // userActiveSubreddit.forEach( function(item){
-    //   activeSubredditString = activeSubredditString + (item[0] + ": "  + item[1].toString() + ". ")
-    // })
-
-    // return activeSubredditString;
-
-    userActiveSubreddit.forEach( function (item) {
-      userSubreddit.push(item[0]); 
-      userCount.push(item[1]);
-    })
-    return [userSubreddit, userCount]
-  } else {
-    return "No Data for User";
-  }
-}
-
-function checkIfInNestedArrayAndIncrementIfTrue(array, item) {
-  for(var i = 0, arrayLength = array.length; i < arrayLength; i++) {
-    if(array[i].indexOf(item) != -1) {
-      array[i][1]++
-      return true;
-    }
-  }
-  return false;
-}
-
-$(function() {
+$( document ).ready(function() {
     console.log("Page Loaded");
+
+    function attachEventListenersToUsers() {
+      $("[data-author] .tagline").each( function(index, currentUserTagline){
+        var currentUsername = currentUserTagline.getElementsByClassName("author")[0];
+
+        if(currentUsername && currentUsername.text) {
+          var userNeedsData = (function() {
+            var wasDataLoaded = true;
+            
+            return {
+              dataLoadedBoolean: function() {
+                return wasDataLoaded
+              },
+              dataLoadedTrue: function() {
+                wasDataLoaded = false;
+              }
+            }
+          }())
+
+          var contentBox = document.createElement('span');
+          contentBox.className = "usercontext";
+          contentBox.innerHTML = "💣";
+
+          currentUserTagline.appendChild(contentBox);
+
+          var userContext = document.createElement('span');
+          userContext.className = "tooltiptext";
+
+          $(contentBox).mouseenter(function(){
+            if(userNeedsData.dataLoadedBoolean()) {
+              getRedditUserData(currentUsername.text, currentUserTagline, contentBox, userContext, userNeedsData);
+            }
+          })
+        };
+      })
+    }
+
+    function getRedditUserData(username, currentUserTagline, contentBox, userContext, userNeedsData) {
+      var overviewData = [],
+          upvotedData = [],
+          savedData = [];
+
+      $.when(
+        $.get("https://www.reddit.com/user/" + username + "/overview.json?limit=10", function(overview, status){
+            if(status === "success") {
+              overviewData = overview.data.children;
+            }
+          })
+
+        // $.get("https://www.reddit.com/user/" + username + "/upvoted.json?limit=10", function(upvoted, status){
+        //     if(status === "success") {
+        //       upvotedData = upvoted.data.children;
+        //     }
+        //   }),
+
+        // $.get("https://www.reddit.com/user/" + username + "/saved.json?limit=10", function(saved, status){
+        //     if(status === "success") {
+        //       savedData = saved.data.children;
+        //     }
+        //   })
+      ).then( function(){
+        var chartArea = document.createElement("canvas");
+        // make sure userNeedsData is set to true so the information isn't loaded again on hover
+        userNeedsData.dataLoadedTrue()
+
+        chartArea.width = "300";
+        chartArea.height = "300";
+
+        var chartUserData = parseUserDataForSubreddits(overviewData.concat(upvotedData, savedData));
+
+        var subbredditChart = new Chart(chartArea, {
+          type: 'doughnut',
+          data: {
+              labels: chartUserData[0],
+              datasets: [
+                  {
+                      data: chartUserData[1],
+                      backgroundColor: [
+                    "#F9C80E",
+                    "#F86624",
+                    "#EA3546",
+                    "#662E9B",
+                    "#43BCCD",
+                    "#3D315B",
+                    "#444B6E",
+                    "#708B75",
+                    "#9AB87A",
+                    "#F8F991"
+
+                ]
+                  }
+              ]
+            },
+          options: { responsive: false }
+        })
+
+        userContext.appendChild(chartArea)
+        contentBox.appendChild(userContext);
+        currentUserTagline.appendChild(contentBox);
+      })
+    }
+
+    function parseUserDataForSubreddits(userData) {
+      var userActiveSubreddit = [],
+          currentSubreddit,
+          activeSubredditString = "";
+
+      for(var i = 0, userDataLength = userData.length; i < userDataLength; i++) {
+        currentSubreddit = userData[i].data.subreddit;
+        
+        if(!checkIfInNestedArrayAndIncrementIfTrue(userActiveSubreddit, currentSubreddit)) {
+          userActiveSubreddit.push([currentSubreddit, 1]);
+        }
+      }
+
+      userActiveSubreddit.sort(function(a, b){return a[1] < b[1]})
+      if(userActiveSubreddit.length) {
+        var userSubreddit = [],
+            userCount = [];
+        // userActiveSubreddit.forEach( function(item){
+        //   activeSubredditString = activeSubredditString + (item[0] + ": "  + item[1].toString() + ". ")
+        // })
+
+        // return activeSubredditString;
+
+        userActiveSubreddit.forEach( function (item) {
+          userSubreddit.push(item[0]); 
+          userCount.push(item[1]);
+        })
+        return [userSubreddit, userCount]
+      } else {
+        return "No Data for User";
+      }
+    }
+
+    function checkIfInNestedArrayAndIncrementIfTrue(array, item) {
+      for(var i = 0, arrayLength = array.length; i < arrayLength; i++) {
+        if(array[i].indexOf(item) != -1) {
+          array[i][1]++
+          return true;
+        }
+      }
+      return false;
+    }
+
+
     attachEventListenersToUsers();
 });
